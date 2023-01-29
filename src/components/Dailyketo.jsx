1777dda928894765befc576bf860fed9702ef7c8
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2023-01-29 16:40:42 +0100

    Add SplideSlide

--- src/components/Dailyketo.jsx
+++ src/components/Dailyketo.jsx
@@ -1,51 +1,82 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 const Dailyketo = () => {
-
-    const [daily, setDaily] = useState([]);
+  const [daily, setDaily] = useState([]);
 
   useEffect(() => {
     getDaily();
   }, []);
 
   const getDaily = async () => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
     const data = await api.json();
-    setDaily(data.recipes)
-   
-      
-      
+    setDaily(data.recipes);
   };
 
-  return <div>
-    {daily.map((recipe) =>{
-        return(
-            <Wrapper key={recipe.id}>
-                <h3>Radndom recipes</h3>
-                {daily.map((recipe) => {
-                    return(
-                        <Card>
-                           <p>{recipe.title}</p>
-                           <img src={recipe.image} alt={recipe.title} />
-                        </Card>
-                    )
-                })}
-            </Wrapper>
-        );
-    })}
-  </div>;
+  return (
+    <div>
+      <Wrapper>
+        <h3>Radndom recipes</h3>
+        <Splide options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: 'free',
+            gap: '5rem'
+
+
+        }}>
+          {daily.map((recipe) => {
+            return (
+              <SplideSlide>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+    </div>
+  );
 };
 const Wrapper = styled.div`
-    margin: 5rem 0rem
-`
+  margin: 4rem 0rem;
+`;
 const Card = styled.div`
-    min-height: 25rem;
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  img {
     border-radius: 2rem;
-    overflow: hidden;
-    img{
-        border-radius: 2rem;
-    }
-`
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+   p{
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
 export default Dailyketo;
